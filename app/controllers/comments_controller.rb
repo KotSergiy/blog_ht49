cd8ac5044acec34fedd4568752222a6f53796539
KotSergiy <ks77vl@gmail.com>
2021-01-15 22:14:06 +0200

    Refactor comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,16 +24,15 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
     post=Post.find(params[:post_id])
-    post.comments << @comment
+    @comment=post.comments.build(comment_params)
 
     respond_to do |format|
-      if post.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+      if @comment.save
+        format.html { redirect_to post, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
-        format.html { render :new }
+        format.html { render 'post/show' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
